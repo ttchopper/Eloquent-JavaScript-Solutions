6e0e7169e31bcc7c9a63d3c1a22d0122364e4197
andrew <twinklechopper@gmail.com>
2016-03-09 23:19:55 +0200

    Finish ex3 ch5

---
+++ 06.Sequence Interface.js
@@ -0,0 +1,40 @@
+function logFive(sequence) {
+  while (sequence.state < 4) {
+    if (sequence.state === sequence.end) {
+      return;
+    }
+    console.log(sequence.next());
+  }
+}
+
+function ArraySeq(arr) {
+    this.state = -1;
+    this.arr = arr;
+    this.end = this.arr.length - 1;
+}
+ArraySeq.prototype.next = function () {
+        this.state += 1;
+        return this.arr[this.state];
+};
+
+function RangeSeq(from, to) {
+	this.state = -1;
+    this.from = from;
+    this.end = to - from;
+}
+
+RangeSeq.prototype.next = function () {
+  this.state += 1;
+  return this.from + this.state;
+};
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+
+logFive(new RangeSeq(99, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
